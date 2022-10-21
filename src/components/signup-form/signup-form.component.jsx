a5d6bdd56c63771a7ab820f2138d50f202155f64
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2022-10-21 21:40:16 +0300

    :got some new functionalities

--- src/components/signup-form/signup-form.component.jsx
+++ src/components/signup-form/signup-form.component.jsx
@@ -15,7 +15,7 @@ const defaultFormFields = {
 
 
 const SignUpForm = () => {
-    console.log('hit')
+    console.log('hitt')
     const [formField, setFormField] = useState(defaultFormFields)
     const {displayName, email, password, confirmPassword} = formField
    
