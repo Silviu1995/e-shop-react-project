34e2e16f1a8d9d8f1a81e68637ad80e34c21359e
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2022-11-12 11:11:53 +0200

    :undo

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -24,7 +24,7 @@ const Directory = ({category}) => {
     {
       "id": 4,
       "title": "womens",
-      "imageUrl": "https://www.frasersexperience.com/images/estore_blog_fashionculottes-01.jpg",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png",
       "route":"shop/womens"
     },
     {
