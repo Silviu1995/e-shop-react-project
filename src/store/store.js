d4e5ec324c7a4fd820d511bf7d55a63ef1d5a61f
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2022-11-27 20:12:21 +0200

    :implemented more redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
 import { compose, createStore,applyMiddleware } from "redux";
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger]
+const myMiddleWare = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action)
+    }
+
+    console.log('type: ',action.type)
+    console.log('payload: ', action.payload)
+    console.log('current state: ', store.getState())
+
+    next(action)
+    console.log('next state: ',store.getState())
+}
+const middleWares = [myMiddleWare]
 
 const composeEnhancer = compose(applyMiddleware(...middleWares))
 
